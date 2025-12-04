d586b765d9497671bf4892c6c42f19ffdf1be2d0
saipavanb9-Tempest <saipavanbata200304@gmail.com>
2025-12-04 18:10:53 +0530

    Add files via upload

---
+++ Task1_Assignment2.py
@@ -0,0 +1,29 @@
+"""
+Problem Statement:  Write a Python program that:
+1. 	Takes an integer input from the user.
+2. 	Checks whether the number is even or odd using an if-else statement.
+3. 	Displays the result accordingly.
+"""
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print("Number is even.")
+else:
+    print("Number is odd.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Task2_Assignment2.py
@@ -0,0 +1,19 @@
+"""
+Problem Statement: Write a Python program that:
+1.   Uses a for loop to iterate over numbers from 1 to 50.
+2.   Calculates the sum of all integers in this range.
+3.   Displays the final sum.
+"""
+
+numbers = range(1, 51)
+for x in numbers:
+    answer = sum(numbers)
+print(f"The Sum of Numbers from 1 to 50 is: {answer}")
+
+
+
+
+
+
+
+
